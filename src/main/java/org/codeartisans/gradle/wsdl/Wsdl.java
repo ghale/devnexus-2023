8f83011d6042eb312dbb8458b2e0ac3e1a527c5d
Gary Hale <gary@gradle.com>
2023-04-01 08:57:58 -0400

    Fix configuration cache issues

--- src/main/java/org/codeartisans/gradle/wsdl/Wsdl.java
+++ src/main/java/org/codeartisans/gradle/wsdl/Wsdl.java
@@ -1,48 +1,19 @@
 package org.codeartisans.gradle.wsdl;
 
-import java.io.File;
-import java.io.Serializable;
 import org.gradle.api.Named;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFile;
 
-public class Wsdl implements Named, Serializable {
-
-    private String name;
-    private File wsdl;
-    private String packageName;
-
-    public Wsdl() {}
-
-    public Wsdl( String name ) {
-        this.name = name;
-    }
-
+public interface Wsdl extends Named {
     @Override
-    public String getName() {
-        return name;
-    }
+    @Input
+    String getName();
 
-    public void setName( String name ) {
-        this.name = name;
-    }
+    @InputFile
+    RegularFileProperty getWsdl();
 
-    public File getWsdl() {
-        return wsdl;
-    }
-
-    public void setWsdl( File wsdl ) {
-        this.wsdl = wsdl;
-    }
-
-    public String getPackageName() {
-        return packageName;
-    }
-
-    public void setPackageName( String packageName ) {
-        this.packageName = packageName;
-    }
-
-    @Override
-    public String toString() {
-        return name + " WSDL to " + packageName;
-    }
+    @Input
+    Property<String> getPackageName();
 }
--- src/main/java/org/codeartisans/gradle/wsdl/WsdlToJava.java
+++ src/main/java/org/codeartisans/gradle/wsdl/WsdlToJava.java
@@ -6,40 +6,33 @@ import java.util.List;
 import javax.inject.Inject;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.NamedDomainObjectContainer;
-import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.model.ObjectFactory;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.OutputDirectory;
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.*;
 import org.gradle.workers.WorkQueue;
 import org.gradle.workers.WorkerExecutor;
 
 public abstract class WsdlToJava extends DefaultTask {
-    private Configuration jaxwsToolsConfiguration;
-    private final NamedDomainObjectContainer<Wsdl> wsdls = getProject().getObjects().domainObjectContainer(Wsdl.class);
+    private FileCollection jaxwsToolsConfiguration;
 
     @InputFiles
-    public Configuration getJaxwsToolsConfiguration() {
+    public FileCollection getJaxwsToolsConfiguration() {
         return jaxwsToolsConfiguration;
     }
 
-    public void setJaxwsToolsConfiguration( Configuration jaxwsToolsConfiguration ) {
+    public void setJaxwsToolsConfiguration( FileCollection jaxwsToolsConfiguration ) {
         this.jaxwsToolsConfiguration = jaxwsToolsConfiguration;
     }
 
-    @Input
-    public NamedDomainObjectContainer<Wsdl> getWsdls() {
-        return wsdls;
-    }
+    @Nested
+    abstract public NamedDomainObjectContainer<Wsdl> getWsdls();
 
     @InputFiles
     public FileCollection getWsdlFiles() {
-        ConfigurableFileCollection wsdlFiles = getProject().files();
-        for( Wsdl wsdl : wsdls ) {
+        ConfigurableFileCollection wsdlFiles = getObjectFactory().fileCollection();
+        for( Wsdl wsdl : getWsdls() ) {
             wsdlFiles.from( wsdl.getWsdl() );
         }
         return wsdlFiles;
@@ -51,32 +44,37 @@ public abstract class WsdlToJava extends DefaultTask {
     @TaskAction
     public void processWsdls() {
         WorkQueue queue = getWorkerExecutor().classLoaderIsolation(spec -> spec.getClasspath().from(jaxwsToolsConfiguration));
-        wsdls.forEach(wsdl ->
+        getWsdls().forEach(wsdl ->
             queue.submit(WsImportWorkAction.class,
                     wsImportParameters -> wsImportParameters.getArguments().addAll(wsImportArgumentsFor(wsdl))));
     }
 
     private List<String> wsImportArgumentsFor( Wsdl wsdl ) {
+        File wsdlFile = wsdl.getWsdl().getAsFile().get();
+
         List<String> arguments = new ArrayList<>();
         if( wsdl.getPackageName() != null ) {
             arguments.add( "-p" );
-            arguments.add( wsdl.getPackageName() );
+            arguments.add( wsdl.getPackageName().get() );
         }
         arguments.add( "-wsdllocation" );
-        arguments.add( wsdl.getWsdl().getName() );
+        arguments.add( wsdlFile.getName() );
         arguments.add( "-s" );
         arguments.add(getOutputDirectory().getAsFile().get().getAbsolutePath() );
         arguments.add( "-extension" );
         arguments.add( "-Xnocompile" );
-        if( getProject().getLogger().isDebugEnabled() ) {
+        if(getLogger().isDebugEnabled()) {
             arguments.add( "-Xdebug" );
         } else {
             arguments.add( "-quiet" );
         }
-        arguments.add( wsdl.getWsdl().getAbsolutePath() );
+        arguments.add( wsdlFile.getAbsolutePath() );
         return arguments;
     }
 
     @Inject
     protected abstract WorkerExecutor getWorkerExecutor();
+
+    @Inject
+    protected abstract ObjectFactory getObjectFactory();
 }
